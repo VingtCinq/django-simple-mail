15cc825f298722cd5976c35dbe7fd15259a5d5c5
Jufik <julien.kieffer@gadz.org>
2018-09-05 11:32:54 +0200

    issue #4 to be tested

--- simple_mail/apps.py
+++ simple_mail/apps.py
@@ -1,7 +1,7 @@
 from __future__ import unicode_literals
 
 from django.apps import AppConfig
-
+from django.conf import settings
 from simple_mail.mailer import autodiscover
 
 
@@ -11,3 +11,18 @@ class SimpleMailConfig(AppConfig):
 
     def ready(self):
         autodiscover()
+
+        if getattr(settings, 'SIMPLE_MAIL_CSS_WARNING', False):
+            from cssutils import profile
+            from cssutils.profiles import Profiles, properties, macros
+            # patch um up
+            properties[Profiles.CSS_LEVEL_2]['-ms-interpolation-mode'] = r'none|bicubic|nearest-neighbor'
+            properties[Profiles.CSS_LEVEL_2]['-ms-text-size-adjust'] = r'none|auto|{percentage}'
+            properties[Profiles.CSS_LEVEL_2]['mso-table-lspace'] = r'0|{num}(pt)'
+            properties[Profiles.CSS_LEVEL_2]['mso-table-rspace'] = r'0|{num}(pt)'
+            properties[Profiles.CSS_LEVEL_2]['-webkit-text-size-adjust'] = r'none|auto|{percentage}'
+            # re-add
+            profile.addProfiles([(Profiles.CSS_LEVEL_2,
+                                  properties[Profiles.CSS_LEVEL_2],
+                                  macros[Profiles.CSS_LEVEL_2]
+                                  )])
